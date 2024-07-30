08c6a2c5258129098964318b9e9abfce93f160c2
Pavithra <pavi2468kuk@gmail.com>
2024-07-30 09:26:54 +0530

    prepared the base VGGnet16 model

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src.lungClassifier import logger
 from src.lungClassifier.pipeline.stage_01 import DataIngestionTrainingPipeline
-
-
+from src.lungClassifier.pipeline.stage_02 import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -17,16 +16,16 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Prepare base model"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
