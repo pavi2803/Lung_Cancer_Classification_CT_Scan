413abc1ddf638875304a264b202130f15e92e7a3
Pavithra <pavi2468kuk@gmail.com>
2024-08-04 18:38:42 +0530

    prediction_updated

--- src/lungClassifier/pipeline/prediction.py
+++ src/lungClassifier/pipeline/prediction.py
@@ -3,13 +3,26 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import streamlit as st
 import os
-
-
+import base64
+from PIL import Image
+from io import BytesIO
 
 class PredictionPipeline:
     def __init__(self, filename):
         self.filename = filename
 
+    def img_to_base64(self, img_path):
+        with open(img_path, "rb") as img_file:
+            return base64.b64encode(img_file.read()).decode("utf-8")
+
+
+    def base64_to_image(self, base64_str):
+        img_data = base64.b64decode(base64_str)
+        img = Image.open(BytesIO(img_data))
+        img = img.convert("RGB")  # Convert image to RGB
+        return img
+
+  
     def predict(self):
         model_path = os.path.join("model_final", "model.h5")
         
@@ -24,15 +37,20 @@ class PredictionPipeline:
             return f"Error: {e}"
 
         try:
-            test_image = image.load_img(self.filename, target_size=(224, 224))
-            test_image = image.img_to_array(test_image)
-            test_image = np.expand_dims(test_image, axis=0)
-            result = np.argmax(model.predict(test_image), axis=1)
+            base64_image = self.img_to_base64(self.filename)
+            img = self.base64_to_image(base64_image)
+            img = img.resize((224, 224))
+            img = image.img_to_array(img)
+            img = np.expand_dims(img, axis=0)
+            img = img / 255.0  # Normalize the image
+
+            predictions = model.predict(img)
+            result = np.argmax(predictions, axis=1)
+            st.write(f"Raw predictions: {predictions}")  # Print raw predictions
         except Exception as e:
             st.error(f"Error processing image: {e}")
             return f"Error: {e}"
 
-        print(result)
         if result[0] == 1:
             prediction = 'Normal'
         else:
@@ -40,7 +58,8 @@ class PredictionPipeline:
         return prediction
 
 # Streamlit UI
-st.title("Image Classification with Keras Model")
+st.title("Lung Cancer Prediction")
+st.subheader("Transfer Learning on VGGNET-16")
 st.write("Upload an image to classify it as Normal or Adenocarcinoma Cancer.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
