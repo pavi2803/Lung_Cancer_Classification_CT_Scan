0760339697e5a7980a3add6f8e037ed4585bb28d
pavi2803 <pavi2468kuk@gmail.com>
2025-07-10 19:27:11 -0700

    update

--- final_app_st.py
+++ final_app_st.py
@@ -39,6 +39,22 @@ class LungDetector:
 
 
 
+
+def cancer_classifier(input_shape=(224, 224, 3), num_classes=2):
+    base_model = tf.keras.applications.VGG16(
+        input_shape=input_shape,
+        include_top=False,
+        weights="imagenet"
+    )
+    base_model.trainable = False
+
+    x = tf.keras.layers.Flatten()(base_model.output)
+    outputs = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
+    model = tf.keras.Model(inputs=base_model.input, outputs=outputs)
+
+    return model
+
+
 # Main pipeline
 class PredictionPipeline:
     def __init__(self, image_bytes):
@@ -74,14 +90,18 @@ class PredictionPipeline:
             #old
             # cancer_model = load_model(self.cancer_model_path)
 
-            with open("artifacts/training/model.json", "r") as json_file:
-                model_json = json_file.read()
+            # with open("artifacts/training/model.json", "r") as json_file:
+            #     model_json = json_file.read()
 
-            cancer_model = model_from_json(model_json)
-            cancer_model.load_weights("artifacts/training/model.weights.h5")
+            # cancer_model = model_from_json(model_json)
+            # cancer_model.load_weights("artifacts/training/model.weights.h5")
 
             # cancer_model = lung_classifier(input_shape=(224,224,3), num_classes=2, freeze_all=True)
             # cancer_model.load_weights("artifacts/training/model.h5")
+
+            # Rebuild and load weights
+            cancer_model = cancer_classifier()
+            cancer_model.load_weights("artifacts/training/model.weights.h5")
 
             predictions = cancer_model.predict(keras_img)
             result = np.argmax(predictions, axis=1)
